3e23466eacaba6c3b610389c5f508a80216d6217
Gubriel <gustavocaspin@proton.me>
2026-03-04 17:56:30 -0300

    Added animation keyframes and new colors to dashboard

--- ctf-points-system/resources/views/dashboard.blade.php
+++ ctf-points-system/resources/views/dashboard.blade.php
@@ -1,16 +1,11 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+        <div class="max-w-7xl mx-auto sm:px-6 mt-10 lg:px-8">
+            <div class="bg-black border-4 border-green-500 overflow-hidden shadow-sm">
+                <div class="p-4 text-center text-2xl text-green-500">
+                    {{ __("LIDERES") }}
                 </div>
+                <div class="h-16 w-full -translate-x-full animate-shimmer bg-gradient-to-r from-transparent via-green-500/100 to-transparent blur-sm"></div>
             </div>
         </div>
     </div>
--- ctf-points-system/tailwind.config.js
+++ ctf-points-system/tailwind.config.js
@@ -14,6 +14,19 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+                animation: {
+                glow: "glow 2s ease-in infinite alternate",
+                shimmer: "shimmer 3s cubic-bezier(0.4,0,0.2,1) infinite",
+            },
+            keyframes: {
+                glow: {
+                "0%": { boxShadow: "0 0 20px #22C55E" },
+                "100%": { boxShadow: "0 0 40px #22C55E" },
+                },
+                shimmer: {
+                    "100%": { transform: "translateX(100%)" },
+                },
+            },
         },
     },
 
